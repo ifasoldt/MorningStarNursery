25d1a2dd0265bf9c1868d0cf93cbf47054dbadc4
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-09-29 22:17:18 -0400

    images?

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.serve_static_assets = true
 
 module Morningstarnursery
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
   end
 end
